809cab836cde7781f14ecdd5633bbbe8c6d4de1b
Alireza Mohammadi <mohammadialireza238@gmail.com>
2023-12-26 12:11:03 +0100

    Adds entry point to package.json.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: true,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [],
+        },
+      },
+    },
+  },
   plugins: [
     vue(),
   ],
