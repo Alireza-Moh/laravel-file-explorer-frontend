af568e8bd3c45fa4d80455afe85ef1d1a252b0d4
Alireza Mohammadi <mohammadialireza238@gmail.com>
2024-01-07 17:46:12 +0100

    Extracts test data into json files.

--- src/tests/components/DiskList.test.js
+++ src/tests/components/DiskList.test.js
@@ -2,6 +2,11 @@ import {flushPromises, mount} from "@vue/test-utils";
 import DiskList from "@/components/DiskList.vue";
 import {createTestingPinia} from "@pinia/testing";
 import {useDisksStore} from "@/stores/disksStore.js";
+import diskApiResponseTestData from "@/tests/testData/diskApiResponseTestData.json";
+import diskDirsStoreTestData from "@/tests/testData/stores/diskDirsStoreTestData.json";
+import dirItemsStoreTestData from "@/tests/testData/stores/dirItemsStoreTestData.json";
+import settingsStoreTestData from "@/tests/testData/stores/settingsStoreTestData.json";
+import disksStoreTestData from "@/tests/testData/stores/disksStoreTestData.json";
 
 describe.concurrent("DiskList component", () => {
     let wrapper, disksStore, $http;
@@ -9,79 +14,7 @@ describe.concurrent("DiskList component", () => {
     beforeEach(() => {
         $http = {
             get: vi.fn().mockImplementation(() => {
-                return Promise.resolve({
-                    result: {}
-               /*     result: {
-                        dirs: [
-                            {
-                                diskName: "web",
-                                label: "android",
-                                path: "android",
-                                subDir: [
-                                    {
-                                        diskName: "web",
-                                        label: "forTesting",
-                                        path: "android/forTesting",
-                                        subDir: []
-                                    }
-                                ]
-                            },
-                            {
-                                diskName: "web",
-                                label: "ios",
-                                path: "ios",
-                                subDir: [
-                                    {
-                                        diskName: "web",
-                                        label: "forRealse",
-                                        path: "ios/forRealse",
-                                        subDir: [
-                                            {
-                                                diskName: "web",
-                                                label: "DB IMAGES",
-                                                path: "ios/forRealse/DB IMAGES",
-                                                subDir: [
-                                                    {
-                                                        diskName: "web",
-                                                        label: "svgs",
-                                                        path: "ios/forRealse/DB IMAGES/svgs",
-                                                        subDir: []
-                                                    }
-                                                ]
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
-                        ],
-                        selectedDir: "android",
-                        selectedDirPath: "android",
-                        selectedDirItems: [
-                            {
-                                diskName: "mobile",
-                                dirName: "android",
-                                name: "4576_001ss.jpg",
-                                size: 87.91,
-                                lastModified: "2018-11-27 21:19:34",
-                                type: "file",
-                                path: "android/4576_001 (2021_08_10 13_17_28 UTC).jpg",
-                                url: "http://localhost:8084/storage/mobile/android/4576_001ss.jpg",
-                                extension: "jpg"
-                            },
-                            {
-                                diskName: "mobile",
-                                dirName: "android",
-                                name: "45A13B56photo.jpg",
-                                size: 263.98,
-                                lastModified: "2020-10-30 23:54:32",
-                                type: "file",
-                                path: "android/45A13B56photo.jpg",
-                                url: "http://localhost:8084/storage/mobile/android/45A13B56photo.jpg",
-                                extension: "jpg"
-                            }
-                        ]
-                    }*/
-                });
+                return Promise.resolve(diskApiResponseTestData);
             })
         }
 
@@ -93,168 +26,10 @@ describe.concurrent("DiskList component", () => {
                 plugins: [
                     createTestingPinia({
                         initialState: {
-                            disks: {
-                                disks: [
-                                    "tests",
-                                    "images",
-                                    "web"
-                                ]
-                            },
-                            settings: {
-                                defaultFileExplorerViewData: {
-                                    selectedDisk: "tests",
-                                    selectedDir: "android",
-                                    selectedDirPath: "android",
-                                    dirsForSelectedDisk: [
-                                        {
-                                            diskName: "web",
-                                            label: "android",
-                                            path: "android",
-                                            subDir: [
-                                                {
-                                                    diskName: "web",
-                                                    label: "forTesting",
-                                                    path: "android/forTesting",
-                                                    subDir: []
-                                                }
-                                            ]
-                                        },
-                                        {
-                                            diskName: "web",
-                                            label: "ios",
-                                            path: "ios",
-                                            subDir: [
-                                                {
-                                                    diskName: "web",
-                                                    label: "forRealse",
-                                                    path: "ios/forRealse",
-                                                    subDir: [
-                                                        {
-                                                            diskName: "web",
-                                                            label: "DB IMAGES",
-                                                            path: "ios/forRealse/DB IMAGES",
-                                                            subDir: [
-                                                                {
-                                                                    diskName: "web",
-                                                                    label: "svgs",
-                                                                    path: "ios/forRealse/DB IMAGES/svgs",
-                                                                    subDir: []
-                                                                }
-                                                            ]
-                                                        }
-                                                    ]
-                                                }
-                                            ]
-                                        }
-                                    ],
-                                    selectedDirItems: [
-                                        {
-                                            diskName: "mobile",
-                                            dirName: "android",
-                                            name: "4576_001ss.jpg",
-                                            size: 87.91,
-                                            lastModified: "2018-11-27 21:19:34",
-                                            type: "file",
-                                            path: "android/4576_001 (2021_08_10 13_17_28 UTC).jpg",
-                                            url: "http://localhost:8084/storage/mobile/android/4576_001ss.jpg",
-                                            extension: "jpg"
-                                        },
-                                        {
-                                            diskName: "mobile",
-                                            dirName: "android",
-                                            name: "45A13B56photo.jpg",
-                                            size: 263.98,
-                                            lastModified: "2020-10-30 23:54:32",
-                                            type: "file",
-                                            path: "android/45A13B56photo.jpg",
-                                            url: "http://localhost:8084/storage/mobile/android/45A13B56photo.jpg",
-                                            extension: "jpg"
-                                        }
-                                    ]
-                                }
-                            },
-                            diskDirs: {
-                                dirs: [
-                                    {
-                                        dirs: [
-                                            {
-                                                diskName: "web",
-                                                label: "android",
-                                                path: "android",
-                                                subDir: [
-                                                    {
-                                                        diskName: "web",
-                                                        label: "forTesting",
-                                                        path: "android/forTesting",
-                                                        subDir: []
-                                                    }
-                                                ]
-                                            },
-                                            {
-                                                diskName: "web",
-                                                label: "ios",
-                                                path: "ios",
-                                                subDir: [
-                                                    {
-                                                        diskName: "web",
-                                                        label: "forRealse",
-                                                        path: "ios/forRealse",
-                                                        subDir: [
-                                                            {
-                                                                diskName: "web",
-                                                                label: "DB IMAGES",
-                                                                path: "ios/forRealse/DB IMAGES",
-                                                                subDir: [
-                                                                    {
-                                                                        diskName: "web",
-                                                                        label: "svgs",
-                                                                        path: "ios/forRealse/DB IMAGES/svgs",
-                                                                        subDir: []
-                                                                    }
-                                                                ]
-                                                            }
-                                                        ]
-                                                    }
-                                                ]
-                                            }
-                                        ],
-                                        diskName: "web",
-                                        selectedDir: "ios"
-                                    }
-                                ]
-                            },
-                            dirItems: {
-                                data: [
-                                    {
-                                        dirItems: [
-                                            {
-                                                diskName: "mobile",
-                                                dirName: "android",
-                                                name: "4576_001ss.jpg",
-                                                size: 87.91,
-                                                lastModified: "2018-11-27 21:19:34",
-                                                type: "file",
-                                                path: "android/4576_001 (2021_08_10 13_17_28 UTC).jpg",
-                                                url: "http://localhost:8084/storage/mobile/android/4576_001ss.jpg",
-                                                extension: "jpg"
-                                            },
-                                            {
-                                                diskName: "mobile",
-                                                dirName: "android",
-                                                name: "45A13B56photo.jpg",
-                                                size: 263.98,
-                                                lastModified: "2020-10-30 23:54:32",
-                                                type: "file",
-                                                path: "android/45A13B56photo.jpg",
-                                                url: "http://localhost:8084/storage/mobile/android/45A13B56photo.jpg",
-                                                extension: "jpg"
-                                            }
-                                        ],
-                                        diskName: "web",
-                                        dirName: "ios",
-                                    }
-                                ]
-                            }
+                            disks: disksStoreTestData,
+                            settings:settingsStoreTestData,
+                            diskDirs: diskDirsStoreTestData,
+                            dirItems: dirItemsStoreTestData
                         }
                     })
                 ]
